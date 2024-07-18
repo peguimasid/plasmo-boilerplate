f1898febaa56f426a8d3169ac251e0591dae4511
Guilhermo Masid <guilhermomasid@gmail.com>
2024-07-18 16:30:18 -0300

    chore: test pre commit hook

--- src/content.tsx
+++ src/content.tsx
@@ -1,24 +1,24 @@
-import cssText from 'data-text:@/index.css'
-import type { PlasmoCSConfig } from 'plasmo'
+import cssText from "data-text:@/index.css";
+import type { PlasmoCSConfig } from "plasmo";
 
-import { CountButton } from '@/components/count-button'
+import { CountButton } from "@/components/count-button";
 
 export const config: PlasmoCSConfig = {
-  matches: ['https://www.google.com/*'],
-}
+  matches: ["https://www.google.com/*"],
+};
 
 export const getStyle = () => {
-  const style = document.createElement('style')
-  style.textContent = cssText
-  return style
-}
+  const style = document.createElement("style");
+  style.textContent = cssText;
+  return style;
+};
 
 const PlasmoOverlay = () => {
   return (
     <div className="fixed right-8 top-32 z-50 flex">
       <CountButton />
     </div>
-  )
-}
+  );
+};
 
-export default PlasmoOverlay
+export default PlasmoOverlay;
